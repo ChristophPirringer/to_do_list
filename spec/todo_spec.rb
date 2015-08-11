1e9bcc1f93ecf5828d6661598e4bf5277cef9bf1
ryan broughan & Christoph Pirringer <student@epicodus.com>
2015-08-11 09:21:54 -0700

    save tasks into @@all_tasks array, add specs to clear that array with .clear method

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -4,11 +4,6 @@ require("todo")
 
 describe(Task) do
 
-  describe(".all")do
-    it("should hopefully please be empty??") do
-      expect(Task.all()).to(eq([]))
-    end
-  end
 
   before() do
     @test_task = Task.new("fry your brain")
@@ -17,6 +12,26 @@ describe(Task) do
   describe("#description")do
     it("lets you give a description of your task")do
       expect(@test_task.description()).to(eq("fry your brain"))
+    end
+  end
+
+  describe(".all")do
+    it("should hopefully please be empty??") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a task to the array of tasks') do
+      @test_task.save()
+      expect(Task.all()).to(eq([@test_task]))
+    end
+  end
+
+  describe(".clear") do
+    it("empties out all of the saved tasks from the @@all_tasks array")do
+      Task.clear()
+      expect(Task.all()).to(eq([]))
     end
   end
 
